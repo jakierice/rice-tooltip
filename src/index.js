e09650b934d105873c9fb5cbe409003f39a2850a
Jakie W. Rice <jakie.rice@gmail.com>
2018-12-12 07:58:11 -0500

    remove uneccessary files, add mock story

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default class Tooltip extends Component {
+  render() {
+    return (
+      <h1>THIS IS WILL BE THE TOOLTIP ANCHOR</h1>)
+  }
+}
